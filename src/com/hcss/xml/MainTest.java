b8c5e7cdfcc509badd72ef7e53d86bcae00b2db2
yuxiwen <yu_yuchengfeng@163.com>
2016-11-21 12:50:04 +0800

    测试

--- src/com/hcss/xml/MainTest.java
+++ src/com/hcss/xml/MainTest.java
@@ -6,7 +6,6 @@ import java.util.Map;
 
 import javax.sql.DataSource;
 
-import com.hcss.xml.biz.XmlParseBiz;
 import com.hcss.xml.dao.DataSourceFactory;
 import com.hcss.xml.service.XmlParseService;
 import com.hcss.xml.service.impl.XmlParseServiceImpl;
@@ -56,41 +55,8 @@ public class MainTest {
 
         // -----------------------华丽丽的分割线-------------------------------------
 
-//        // 多行sql
-////        String sql = "insert into A(a1,a2,a3) values(#ASTNO#,#ACODE_ABTSC#,#AESTR#)";
-//
-//        // 行转列
-////        String sql = "insert into B(b1,b2,b3,b4,b5) values(#ASTNO0#,#ACODE0_ABTSC0#,#ACODE1_ABTSC1#,#AESTR0#,#AESTR1#)";
-//
-//        String sql = "update B set b4 = #ACODE0_ABTSC0#, b5 = #ACODE1_ABTSC1# where b1 = #ASTNO0#";
-//
-//
-//        try {
-//
-////            map = SqlParseUtils.parseMap(map,"DFIE");
-//
-//            // 单行sql
-//            sql = SqlParseUtils.singleParse(sql, map);
-//
-//            System.out.println(sql);
-//
-//            // 多行sql
-////            List<String> sqlList = SqlParseUtils.multiParse(sql, map);
-////
-////            for (String sql1 : sqlList)  System.out.println(sql1);
-//
-//        } catch (Exception e) {
-//
-//            e.printStackTrace();
-//        }
 
-
-        // -----------------------华丽丽的分割线-------------------------------------
-
-
-        XmlParseBiz xmlParseBiz = new XmlParseBiz(dataSource);
-
-        xmlParseBiz.execByFile(file);
+        xmlParseService.execByFile(file);
 
 
 
